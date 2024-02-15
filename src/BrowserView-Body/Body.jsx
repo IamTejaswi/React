3010d5cc506d7b55822a2a6df7b3f46d1faee107
Tejaswi <201220014@nitdelhi.ac.in>
2024-02-15 18:47:42 +0530

    added project

---
+++ src/BrowserView-Body/Body.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Body.css";
+import Left from "./Left/Left.jsx";
+ 
+
+export default function Body() {
+  return (
+    <div className="container">
+      <Left></Left>
+       
+    </div>
+  );
+}
